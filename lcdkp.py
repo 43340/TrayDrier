e8b7250a4a2d40f1e81125fa39e144fe6d49ac74
43340 <arvinjohnyadao625@gmail.com>
2019-02-21 14:49:28 +0800

    LCD + Keypad data input finished. Will finalize the flow of the program later today

--- lcdkp.py
+++ lcdkp.py
@@ -4,8 +4,12 @@ import RPi.GPIO as GPIO
 import time
 import requests
 import json
+import datetime
 
 GPIO.setmode(GPIO.BCM)
+
+lcd = lcddriver.lcd()
+lcd.lcd_clear()
 
 MATRIX = [
     ['1','2','3','A'],
@@ -26,8 +30,15 @@ for j in range(4):
     GPIO.output(COL_PINS[j], 1)
 
 
-def getKey():
+def getKey(prompt="", prompt2=""):
     finput = ""
+
+    if(prompt == "" and prompt2 == ""):
+        pass
+    else:
+        lcd.lcd_clear()
+        lcd.lcd_display_string(prompt, 1)
+        lcd.lcd_display_string(prompt2, 2)
 
     try:
         GPIO.output(12, 1)
@@ -44,30 +55,58 @@ def getKey():
                     if GPIO.input (ROW_PINS[i]) == 0:
                         key = (MATRIX[i][j])
 
-                        if (key=="#"):
-                            print(finput)
-                            lcd.lcd_clear()
-                            lcd.lcd_display_string("Success", 1)
-                            GPIO.cleanup() # Remove this during production/cleanup
+                        if (key=="A"):
+                            time.sleep(0.3)
                             return finput
-                        elif (key=="*"):
+                        elif (key=="B"):
+                            finput = finput[:-1]
+                            lcd.lcd_clear()
+                            lcd.lcd_display_string(prompt, 1)
+                            lcd.lcd_display_string(prompt2, 2)
+                            lcd.lcd_display_string(finput, 3)
+                            time.sleep(0.3)
+                        elif (key=="C"):
                             finput = ""
                             lcd.lcd_clear()
-                        elif (key=="A"):
-                            ctemp, chum = getTempAndHum()
-                            lcd.lcd_display_string("Temp: " + str(ctemp), 2)
-                            lcd.lcd_display_string("Hum: " + str(chum), 3)
+                        elif (key=="D"):
+                            getTempAndHum()
+                        elif (key=="#"):
+                            return "#"
                         else:
                             finput = finput + key
-                            lcd.lcd_display_string(finput, 1)
-                            time.sleep(0.5)
+                            lcd.lcd_display_string(finput, 3)
+                            time.sleep(0.3)
 
-                        while(GPIO.input(ROW_PINS[i]) == 0):
-                            pass
+                        #while(GPIO.input(ROW_PINS[i]) == 0):
+                        #    pass
                 GPIO.output(COL_PINS[j],1)
             #time.sleep(0.2)
     except KeyboardInterrupt:
         GPIO.cleanup()
+
+
+### get functions ###
+
+
+def get_set_temp():
+    prompt = "Please enter the set"
+    prompt2 = "temp for the process"
+    set_temp = getKey(prompt, prompt2)
+    return set_temp
+
+
+def get_cook_time():
+    prompt = "Enter the desired"
+    prompt2 = "cook time"
+    cook_time = getKey(prompt, prompt2)
+    return cook_time
+
+
+def get_read_interval():
+    prompt = "Enter the interval"
+    prompt2 = "to read data"
+    read_interval = getKey(prompt, prompt2)
+    return read_interval
 
 
 def getTempAndHum():
@@ -75,23 +114,55 @@ def getTempAndHum():
     data = r.json()
     ctemp = data['temperature']
     chum = data['humidity']
+    lcd.lcd_clear()
+    lcd.lcd_display_string("Data", 1)
+    lcd.lcd_display_string("Temp: " + str(ctemp), 3)
+    lcd.lcd_display_string("Hum: " + str(chum), 4)
 
-    return ctemp, chum
+
+def sequence():
+    lcd.lcd_display_string("Tray Dryer", 1)
+    lcd.lcd_display_string("Control System", 2)
+    time.sleep(5)
+    lcd.lcd_clear()
+
+    name = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    set_temp = get_set_temp()
+    print(set_temp)
+    cook_time = get_cook_time()
+    read_interval = get_read_interval()
+
+    lcd.lcd_clear()
+    lcd.lcd_display_string("Confirm", 1)
+    lcd.lcd_display_string(set_temp + "C", 2)
+    lcd.lcd_display_string(cook_time + "mins", 3)
+    lcd.lcd_display_string(read_interval + "sec", 4)
+
+    key = getKey()
+
+    while(key != '#'):
+        key = getKey()
 
 
+    lcd.lcd_clear()
+    lcd.lcd_display_string("Sending...", 1)
+    time.sleep(1)
+    lcd.lcd_clear()
+    lcd.lcd_display_string("Sent", 1)
+    time.sleep(1)
 
-lcd = lcddriver.lcd()
-lcd.lcd_clear()
- 
-lcd.lcd_display_string("Arvin", 1)
-lcd.lcd_display_string("John", 2)
-lcd.lcd_display_string("Yadao", 3)
-lcd.lcd_display_string("BS CpE V", 4)
-time.sleep(5)
-lcd.lcd_clear()
+    lcd.lcd_clear()
+    getTempAndHum()
 
-getKey()
+    return {
+        "name": name,
+        "stemp": set_temp,
+        "ctime": cook_time,
+        "rinte": read_interval
+    }
 
+
+sequence()
 """
 class KeyStore:
     def __init__(self):
